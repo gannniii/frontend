4c44f588e3b043d560994d39db033389539832da
gannniii <kke08kr@gmail.com>
2025-04-11 17:51:19 +0900

    js, html

---
+++ htmlsource/js/ajax/ajax.js
@@ -0,0 +1,17 @@
+// console.log("시작");
+// setTimeout(() => {
+//   console.log("중간");
+// }, 3000);
+// console.log("마무리");
+
+// fetch
+// fetch("서버주소")
+// .then("응답도착 => 적절한 타입으로 변환 후 리턴")
+// .then("도착한응답처리")
+
+// console.log("시작");
+// http://jsonplacehoder.typicode.com/todos/1 요청
+fetch("http://jsonplaceholder.typicode.com/todos/1")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+//   console.log("마무리");
